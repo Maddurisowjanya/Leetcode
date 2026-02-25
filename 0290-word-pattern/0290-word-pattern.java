ed78ae18dd9a647e10e3be041745798233907ced
Maddurisowjanya <maddurisowjanya10@gmail.com>
2026-02-25 11:41:46 +0530

    Time: 1 ms (81.73%), Space: 42.6 MB (88.48%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character,String> map=new HashMap<>();
+        String words[]=s.split(" ");
+        if(pattern.length()!=words.length){
+            return false;
+        }
+        for(int i=0;i<words.length;i++){
+            if(!map.containsKey(pattern.charAt(i))){
+                if(map.containsValue(words[i])){
+                    return false;
+                }
+                map.put(pattern.charAt(i),words[i]);
+            }
+            else{
+                if(!map.get(pattern.charAt(i)).equals(words[i])){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
