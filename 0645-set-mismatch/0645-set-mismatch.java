1fd88f24dc2d9ca7f81a8a8a02666787f5bbdc78
Maddurisowjanya <maddurisowjanya10@gmail.com>
2026-03-02 11:07:29 +0530

    Time: 10 ms (42.3%), Space: 47.6 MB (65.11%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -2,18 +2,18 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         Set<Integer> set = new HashSet<>();
         int n = nums.length;
-        int dup = -1;
+        int duplicate = -1;
         int total = (n * (n + 1)) / 2;
         int sum = 0;
 
         for (int num : nums) {
             if (!set.add(num)) {
-                dup = num;
+                duplicate = num;
                 sum -= num;
             }
             sum += num;
         }
 
-        return new int[] { dup, total - sum };
+        return new int[] { duplicate, total - sum };
     }
 }
