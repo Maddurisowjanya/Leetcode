570daec3f00c4e8cc9879e85ff5b64368d9c36a4
Maddurisowjanya <maddurisowjanya10@gmail.com>
2026-03-17 09:13:49 +0530

    Time: 10 ms (84.67%), Space: 77.8 MB (68.45%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        if (root == null) return true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int prev;
+            if (level % 2 == 0) {
+                prev = Integer.MIN_VALUE; 
+            } else {
+                prev = Integer.MAX_VALUE; 
+            }
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                int val = node.val;
+                if (level % 2 == 0) { 
+                    if (val % 2 == 0 || val <= prev) return false;
+                } else { 
+                    if (val % 2 != 0 || val >= prev) return false;
+                }
+                prev = val;
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            level++;
+        }
+        return true;
+    }
+}
