3b900b9d5dffb57aefd78469998c99ea7560017d
Maddurisowjanya <maddurisowjanya10@gmail.com>
2026-09-06 15:56:20 +0530

    Time: 2 ms (95.13%), Space: 45.5 MB (82.7%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,17 @@
 class Solution {
-
     List<List<Integer>> ans = new ArrayList<>();
-
     public void solve(int i, int[] arr, int target, List<Integer> curr) {
-
         if (target == 0) {
             ans.add(new ArrayList<>(curr));
             return;
         }
-
         if (target < 0 || i == arr.length)
             return;
-
         // 1. Take and stay (reuse current element)
         curr.add(arr[i]);
         solve(i, arr, target - arr[i], curr);
-
         // Backtrack
         curr.remove(curr.size() - 1);
-
         // 3. Skip
         solve(i + 1, arr, target, curr);
     }
